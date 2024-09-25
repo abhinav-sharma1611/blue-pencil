3f200444c06ab32a36266dd2d555efeb952ee485
Apple <apple@Apples-MacBook-Air.local>
2024-09-25 19:26:59 +0530

    Image issue in Footer Resolved

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -90,7 +90,7 @@ export default function Footer() {
                         </a>
                         <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">
                             <Image
-                                src='/Instagram.png'
+                                src='/iit nstagram.png'
                                 alt="Instagram"
                                 width={16}
                                 height={16}
